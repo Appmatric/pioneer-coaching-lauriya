a7a19e32a59a86bbca020c1104d351ed0d1eee90
SumitKumargiri <sumitgiri15434@gmail.com>
2025-11-23 17:48:04 +0530

    image carousel effect added

--- src/PCSyatem/Layouts/NavigationBar.jsx
+++ src/PCSyatem/Layouts/NavigationBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import logo from "../../../public/assets/images/logo-pioneer.png";
+import logo from "../../../public/images/logo-pioneer.png";
 import { Menu, X } from "lucide-react";
 
 const NavigationBar = () => {
